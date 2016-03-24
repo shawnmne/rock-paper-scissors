bf9f10f8943dfdd6c1200c518e9d723a460659ee
Shawn Michaud <shawnmne@hotmail.com>
2016-03-24 11:25:43 -0500

    began work on basic logic

---
+++ lib/rock_paper_scissor.rb
@@ -0,0 +1,22 @@
+require "pry"
+
+#determine_outcome_from_tosses method determines who is the winner based on the rules of rock 
+#   paper scissors
+#
+#+toss1 is the entry player 1 makes and can be a rock, paper, or scissor
+#+toss2 is the entry player 2 makes and can be a rock, paper, or scissor
+#
+#returns outcome determined by the rules of rock paper scissor
+def determine_outcome_from_tosses(toss1, toss2)
+	if toss1 = toss2
+		outcome = "tie"
+	elsif toss1 == "r" && toss2 == "s" ||
+				toss1 == "p"&& toss2 == "r" ||
+				toss1 == "s" && toss2 == "p"
+		outcome = "player 1 wins"
+	elsif toss2 == "r" && toss1 == "s" ||
+				toss2 == "p"&& toss1 == "r" ||
+				toss2 == "s" && toss1 == "p"
+		outcome = "player 2 wins"				
+	end
+end
