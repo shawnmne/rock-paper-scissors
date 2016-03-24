388334a9ed7c8f87edbacea7851f14912535e7f0
Shawn Michaud <shawnmne@hotmail.com>
2016-03-24 14:13:59 -0500

    updating tests

--- tests/rock_paper_scissor_test.rb
+++ tests/rock_paper_scissor_test.rb
@@ -9,4 +9,16 @@ class RockPaperScissorTest < Minitest::Test
 		result = determine_outcome_from_tosses("r", "r")
 		assert_equal("tie", result)
 	end
+
+	#test if player 1 should win
+	def test_player1_should_win
+		result = determine_outcome_from_tosses("r", "s")
+		assert_equal("player 1 wins", result)
+	end
+
+	#test if player 2 should win
+	def test_player2_should_win
+		result = determine_outcome_from_tosses("s", "r")
+		assert_equal("player 2 wins", result)
+	end
 end
