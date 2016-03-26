c12ceeded911b9c1cc7222260b290184c31bb8a4
Shawn Michaud <shawnmne@hotmail.com>
2016-03-26 16:55:03 -0500

    Rule class test complete

---
+++ tests/rule_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+require_relative "../lib/rule.rb"
+
+class RuleTest < Minitest::Test
+
+
+	#test if both players enter same toss
+	def test_both_players_toss_same
+		result = Rule.new.determine_outcome_from_tosses("r", "r")
+		assert_equal("tie", result)
+	end
+
+	#test if player 1 should win
+	def test_player1_should_win
+		result = Rule.new.determine_outcome_from_tosses("r", "s")
+		assert_equal(0, result)
+	end
+
+	#test if player 2 should win
+	def test_player2_should_win
+		result = Rule.new.determine_outcome_from_tosses("s", "r")
+		assert_equal(1, result)
+	end
+
+	#test if
+end
